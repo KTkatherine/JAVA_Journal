3c7e3df8a1ccb2ac73f0d5e22acc3d72610b1342
KTkatherine <123142893+KTkatherine@users.noreply.github.com>
2023-01-30 18:59:43 +1100

    Add files via upload

---
+++ JavaSE_3_31.java
@@ -0,0 +1,28 @@
+public class JavaSE_3_31 {
+    /*
+
+Target:
+  1.Basic Grammar
+  2.Data Structures and Algorithms
+  3.My SQL
+  4.JavaEE (Beginner)
+  5.Spring
+  6.JavaEE (Advanced)
+  7.Java Advanced data structures
+
+ Programming languages:
+
+Some are best at coding =applications that run natively on mobile devices — think Objective-C or Java.
+
+Others are good at encoding logic within a PDF file, or on a web page — think JavaScript.
+
+And still others are best at coding fast applications for virtual machines or running directly on
+the operating system — for many people, that’s C or C++.
+
+Want a general purpose language? Think Python, PHP.
+
+R and Matlab are good for statistics and data analytics. And so-on.
+
+     *
+     */
+}
