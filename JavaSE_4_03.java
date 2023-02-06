37c2b8c71a365667a2d59c56a60a94f2659b3cca
KTkatherine <123142893+KTkatherine@users.noreply.github.com>
2023-02-06 18:22:43 +1100

    Add files via upload

--- JavaSE_4_03.java
+++ JavaSE_4_03.java
@@ -39,11 +39,60 @@ public class JavaSE_4_03 {
         int g =  Integer.parseInt(str);
         System.out.println(g);//123
 
-        String str3 = "1a2b3";
-        int h = integer.parseInt(str3);
-        System.out.println(h);
+        //String str3 = "1a2b3"; ---> str included the non - number
+        //int h = integer.parseInt(str3);
+        //System.out.println(h);//java: cannot find symbol
 
-        
+        /**
+         2.1 Arithmetic operators
+         */
+        //153 How to use modulo to get the ones place, the tens place and the hundreds place. (Calculating residue)
+        System.out.println(153 % 10);//3
+        System.out.println(153 / 10 % 10);//53
+        System.out.println(153 % 100);//53
+        System.out.println(153 % 1);//0
+        System.out.println(153 / 100 % 10);//1
+            // ---> 不想要那几位就直接除以10^n
+        double h = 11.5;
+        int i = 2;
+        System.out.println(h % i);//1.5
+
+        /**
+         2.2 Incremental assignment
+         +=
+            a += 1 <=> a = a + 1
+
+         *=
+            b *= 2 <=> b = b * 2
+         */
+
+        /**
+         2.3 Increment and decrement operators
+         a++ : take the value of a then increment
+         ++a : increment then take the value of a
+         */
+        int j = 1;
+        System.out.println(j++);//1
+        int k = 1;
+        System.out.println(++k);//2
+
+        /**
+         2.4 Both the relational == != > < <= >=and logical operators produce Boolean values.
+
+         2.5 logical operators
+         &&逻辑与 ：Both operands are true and return true, while either is false and returns false.
+         ||逻辑与：Both operands are false and return false, and either is true.
+         ! 取反：Operand true returns false and vice versa.
+
+
+         */
+
+
+
+
+
+
+
 
 
 
