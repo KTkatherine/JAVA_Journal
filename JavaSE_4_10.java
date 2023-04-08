502b6a78b95552911294d870466b646073f90ed0
KTkatherine <123142893+KTkatherine@users.noreply.github.com>
2023-04-08 23:31:55 +1000

    Add files via upload

--- JavaSE_4_10.java
+++ JavaSE_4_10.java
@@ -1,2 +1,11 @@
+import java.util.Arrays;
+
 public class JavaSE_4_10 {
+    // 如何把一个数组对象转为字符串对象
+    public static void main(String[] args) {
+        int[] data = {1,3,5};
+        //JDK内置的工具类
+        String str = Arrays.toString(data);
+        System.out.println(str);
+    }
 }
