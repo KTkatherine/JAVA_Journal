c83484f36a181e18fcd6c72a9bf6c4b7c001ba10
KTkatherine <123142893+KTkatherine@users.noreply.github.com>
2025-04-18 23:58:15 +1000

    Add files via upload

--- JavaSE_7_28.java
+++ JavaSE_7_28.java
@@ -1,7 +1,35 @@
 package JavaSE_7_28;
 
+import java.util.Arrays;
+
 public class JavaSE_7_28 {
     public static void main(String[] args) {
+        int[] array = {100,12,31,14,15};
+        System.out.println(toString(array));
+        Arrays.sort(array,0,2);
+        System.out.println(toString(array));
+
+
+    }
+
+    public static void toString(int[] array) {
+        String ret = "[";
+        for (int i = 0; i < array.length; i++) {
+            ret += array[i];
+            if (i != array.length - 1) {
+                ret += ",";
+            }
+        }
+        ret += "]";
+        return ret;
+
+    }
+
+
+
+
+
+    public static void main1(String[] args) {
         int[] array = {1,2,3,4};
         System.out.println(array[0]);
         System.out.println(array[1]);
@@ -13,8 +41,17 @@ public class JavaSE_7_28 {
         //int[] array1 = new int[10];
 
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);// 1 2 3 4
+            System.out.println(array[i]);// 1 2 3 4//******for在循环遍历的时候是有下标的
         }
+        System.out.println();
+
+        //增强for 循环，for - each
+        for (int x: array
+             ) {
+            System.out.println(x + " ");//1 2 3 4//******for在循环遍历的时候是有下标的
+
+        }
+        System.out.println();
 
     }
 
