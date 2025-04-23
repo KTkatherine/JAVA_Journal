b899493ec35f1b1718826865be49dc378ee1e028
KTkatherine <123142893+KTkatherine@users.noreply.github.com>
2025-04-23 23:56:57 +1000

    Add files via upload

---
+++ JavaSE_7_23.java
@@ -0,0 +1,26 @@
+package JavaSE_7_23;
+
+public class JavaSE_7_23 {
+
+    /*
+    JavaSE -> 数据结构 -> MySQL（数据库） -> JavaEE 初阶 -> JavaEE 进阶
+
+    JDK     Java开发工具
+    JDK-Api ；类似C语言阶段学的MSDN ：安装帮助手册
+    IntelliJ IDEA:Java集成开发环境
+
+    *JDK(Java development Kit):Java 开发工具包，提供给程序员使用，包含了JRE，同时还包含了 编译器Javac与自带的调试工具Jconsole,jstack等。
+    *JRE(Java Runtime Environment):Java 运行环境，包含了JVM,Java 基础类库。是使用Java语言编写程序运行的所需环境。
+    *JVM(Java Virtual machine):Java 虚拟机，运行java 代码。
+
+    ctrl + d 多一行。ctrl + x剪切
+    使用cmd 时：错误：编码GBK的不可映射字符 -- 因为有的电脑是 utf8编码，而cmd上 编码的时候 是 GBK编码。
+        --解决方法：
+        javac HelloWorld.java -encoding utf-8
+        java HelloWorld
+
+
+    * 1.初识Java
+
+    */
+}
